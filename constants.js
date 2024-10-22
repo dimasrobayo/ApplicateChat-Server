0bd33e7dc2b78e623cef7d5086d40398f271e8bc
Dimas Robayo <dimasrobayo@gmail.com>
2024-10-22 16:10:00 -0300

    agregando configuraciones para production

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 export const DB_USER = "";
 export const DB_PASSWORD = "";
-export const DB_HOST = "localhost:27017";
+export const DB_HOST = "204.48.21.226:27017";
 
-export const IP_SERVER = "192.168.50.167";
+export const IP_SERVER = "204.48.21.226";
 
 export const JWT_SECRET_KEY = "gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA";
 
-export const PORT = process.env.PORT || 3977;
+export const PORT = process.env.PORT || 4041;
